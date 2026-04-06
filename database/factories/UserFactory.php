5d131f14ecd6b25ce23ce8584ef313b99205f049
ibrahim tabash <i409360245@gmail.com>
2026-04-06 15:13:59 +0300

    edit UserFactory 5

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,13 +23,13 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'Test User',
+            'email' => 'test' . Str::random(5) . '@mail.com',
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => 'user',
         ];
     }
 

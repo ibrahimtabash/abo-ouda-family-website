8b4842a6f4276fd18d6e81a40351af99a093eaf7
ibrahim tabash <i409360245@gmail.com>
2026-04-09 17:51:20 +0300

    edit professionals respnsive page

--- resources/views/backoffice/professionals/index.blade.php
+++ resources/views/backoffice/professionals/index.blade.php
@@ -22,38 +22,61 @@
                     @if ($professionals->isEmpty())
                         <p>{{ __('No professionals found.') }}</p>
                     @else
-                        <table class="w-full table-auto ">
-                            <thead>
-                                <tr class="text-right px-2 py-1">
-                                    <th class="px-2 py-1">اسم المستخدم</th>
-                                    <th class="px-2 py-1">البريد الإلكتروني</th>
-                                    <th class="px-2 py-1">رقم الجوال</th>
-                                    <th class="px-2 py-1">تاريخ التسجيل</th>
-                                    <th class="px-2 py-1">الإجراءات</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($professionals as $professional)
-                                    <tr class="border-t">
-                                        <td class="px-2 py-2">
-                                            <a href="{{ route('backoffice.users.show', $professional->id) }}"
-                                                class="text-blue-600">{{ $professional->user->name }}</a>
-                                        </td>
-                                        <td class="px-2 py-2">{{ $professional->user->email }}</td>
-                                        <td class="px-2 py-2">{{ $professional->phone_number }}</td>
+                        <div class="flex flex-col gap-4 sm:hidden">
+                            @foreach ($professionals as $professional)
+                                <div class="border border-gray-200 rounded-xl p-4 flex flex-col gap-3">
+                                    <a href="{{ route('backoffice.users.show', $professional->id) }}"
+                                        class="text-blue-600">
+                                        <p>{{ $professional->user->name }}</p>
+                                    </a>
 
-                                        <td class="px-2 py-2">{{ $professional->created_at->diffForHumans() }}</td>
-                                        <td class="text-xs text-muted">
-                                            <p
-                                                class="bg-green-400 animate-pulse text-green-700 font-bold px-2 py-1 rounded w-fit">
-                                                تحت
-                                                الصيانة</p>
-                                        </td>
+                                    <p>{{ $professional->user->email }}</p>
+                                    <p>{{ $professional->phone_number }}</p>
 
+                                    <p
+                                        class="text-xs bg-green-300 animate-pulse text-green-800 font-bold px-2 py-1 rounded w-fit">
+                                        الإجراءات تحت الصيانة
+                                    </p>
+                                </div>
+                            @endforeach
+
+                        </div>
+
+                        <div class="hidden sm:block overflow-x-auto">
+                            <table class="w-full table-auto overflow-x-scroll">
+                                <thead>
+                                    <tr class="text-right px-2 py-1">
+                                        <th class="px-2 py-1">اسم المستخدم</th>
+                                        <th class="px-2 py-1">البريد الإلكتروني</th>
+                                        <th class="px-2 py-1">رقم الجوال</th>
+                                        <th class="px-2 py-1">تاريخ التسجيل</th>
+                                        <th class="px-2 py-1">الإجراءات</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach ($professionals as $professional)
+                                        <tr class="border-t">
+                                            <td class="px-2 py-2">
+                                                <a href="{{ route('backoffice.users.show', $professional->id) }}"
+                                                    class="text-blue-600">{{ $professional->user->name }}</a>
+                                            </td>
+                                            <td class="px-2 py-2">{{ $professional->user->email }}</td>
+                                            <td class="px-2 py-2">{{ $professional->phone_number }}</td>
+
+                                            <td class="px-2 py-2">{{ $professional->created_at->diffForHumans() }}</td>
+                                            <td class="text-xs text-muted">
+                                                <p
+                                                    class="bg-green-400 animate-pulse text-green-700 font-bold px-2 py-1 rounded w-fit">
+                                                    تحت
+                                                    الصيانة</p>
+                                            </td>
+
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+
                     @endif
                 </div>
             </div>
